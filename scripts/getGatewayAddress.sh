67d05ba54778db32b5e0c27c2fd2fe42e26021b5
Ali Orhun Akkirman <divercans@gmail.com>
2017-04-08 23:56:47 +0300

    Update getGatewayAddress.sh

--- scripts/getGatewayAddress.sh
+++ scripts/getGatewayAddress.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+#name: getGatewayAddress
+#author: Generic
+#description: Get Gateway Address
+#version: 1.0
+#root: no
+#category: system-tools
+#distro: debbased, rpmbased
 /sbin/ip route | awk '/default/ { print $3 }'
