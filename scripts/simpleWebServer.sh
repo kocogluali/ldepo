dd49856f15fac15ff7cada0a3646a4ca48ceb149
Ali Orhun Akkirman <divercans@gmail.com>
2017-04-09 00:28:34 +0300

    Create simpleWebServer.sh

---
+++ scripts/simpleWebServer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#name: simpleWebServer
+#author: Generic
+#description: Simple Web Server
+#version: 1.0
+#root: no
+#category: webserver
+#distro: debbased, rpmbased
+python -m SimpleHTTPServer 8000
