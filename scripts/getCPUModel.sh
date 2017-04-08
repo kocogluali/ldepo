fb3ef4a5762d09d3d7188a56343633057861d47d
Ali Orhun Akkirman <divercans@gmail.com>
2017-04-08 23:54:31 +0300

    Update getCPUModel.sh

--- scripts/getCPUModel.sh
+++ scripts/getCPUModel.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
+#name: getCPUModel
+#author: Generic
+#description: Get CPU model
+#version: 1.0
+#root: no
+#category: system-tools
+#distro: debbased, rpmbased
 cat /proc/cpuinfo | grep "model name" | tail -1
