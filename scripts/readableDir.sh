302354fe7ae4b6eeca43b9222771e5953372d681
Ali Orhun Akkirman <divercans@gmail.com>
2017-04-09 08:42:44 +0300

    Create readableDir.sh

---
+++ scripts/readableDir.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#name: makeReadableDir
+#author: Generic
+#description: Make Readable Directory
+#version: 1.0
+#root: yes
+#category: system-tools
+#distro: debbased, rpmbased
+sudo chmod o+r $1
